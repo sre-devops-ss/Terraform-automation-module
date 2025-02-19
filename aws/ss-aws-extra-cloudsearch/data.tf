2e4132a49c7d70ed0a3034a80a16939febfa08b7
ashton <ashton.arlo@supportsages.com>
2025-02-19 16:56:10 +0530

    deleted unused module

--- aws/ss-aws-extra-cloudsearch/data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_route53_zone" "opensearch" {
-  name = var.domain_details.hosted_zone
-}
-data "aws_acm_certificate" "opensearch" {
-  domain = var.domain_details.domain
-}
-
-
-
--- aws/ss-aws-extra-cloudsearch/output.tf
+++
@@ -1,3 +0,0 @@
-output "service_name" {
-  value ="OPENSEARCH"
-}
