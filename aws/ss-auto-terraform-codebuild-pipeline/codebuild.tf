d962b34554fe4498b9cff6ae06eb9461c3791c38
ashton <ashton.arlo@supportsages.com>
2025-03-19 17:28:48 +0530

    add - Terraform Pipeline Module

---
+++ aws/ss-auto-terraform-codebuild-pipeline/codebuild.tf
@@ -0,0 +1,67 @@
+data "aws_iam_role" "codebuild"{
+  name = var.codebuild-role-name
+}
+data "aws_ssm_parameter" "buildspec"{
+  name=var.buildspec-parameter
+}
+
+
+
+resource "aws_cloudwatch_log_group" "build" {
+  name = "codebuild/${var.project}-${var.project_environment}"
+  retention_in_days = var.retention
+  tags = {
+
+    Application = "codebuild/${var.project}-${var.project_environment}"
+  }
+}
+resource "aws_cloudwatch_log_stream" "build" {
+  depends_on = [aws_cloudwatch_log_group.build]
+  log_group_name = aws_cloudwatch_log_group.build.name
+  name           = "${var.project}-build-${var.project_environment}"
+}
+
+resource "aws_codebuild_project" "resource" {
+
+  name         = "${var.project}-build-${var.project_environment}"
+  service_role = data.aws_iam_role.codebuild.arn
+
+  artifacts {
+    type = var.source_type
+  }
+
+  source {
+    type =var.source_type
+    buildspec =data.aws_ssm_parameter.buildspec.value
+    report_build_status = true
+  }
+
+  environment {
+    compute_type    = var.env_compute_type
+    image           = var.env_image
+    type            = var.env_type
+    privileged_mode = true
+
+    dynamic "environment_variable"{
+      for_each =var.env_vars
+      content {
+        name=environment_variable.value.name
+        value=environment_variable.value.value
+      }
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = aws_cloudwatch_log_group.build.name
+      stream_name = aws_cloudwatch_log_stream.build.name
+    }
+  }
+
+  tags = {
+    Name="${var.project}-build"
+    last_tf_change=var.modifiedby
+    env=var.project_environment
+  }
+
+}
---
+++ aws/ss-auto-terraform-codebuild-pipeline/main.tf
@@ -0,0 +1,93 @@
+data "aws_iam_role" "codepipeline" {
+  name = var.role_name
+}
+data "aws_ssm_parameter" "kms-enc-id"{
+  name=var.encrypt-id
+}
+data "aws_ssm_parameter" "pipeline-artifacts"{
+  name=var.artifact-bucket
+}
+
+resource "aws_codepipeline" "resource" {
+  depends_on = [data.aws_iam_role.codepipeline]
+  name     = "${var.project}-${var.project_environment}-pipeline"
+  role_arn = data.aws_iam_role.codepipeline.arn
+  artifact_store {
+    location = data.aws_ssm_parameter.pipeline-artifacts.value
+    type     = var.artifact-type
+    encryption_key {
+      id   =data.aws_ssm_parameter.kms-enc-id.value
+      type = var.encrypt_type
+    }
+  }
+
+  stage {
+    name = var.source_name
+    action {
+      category = var.source_category
+      name     = var.source_name
+      owner    = var.source_owner
+      provider = var.source_provider
+      version  = var.source_version
+      role_arn =var.cross-role_arn!=""?var.cross-role_arn:data.aws_iam_role.codepipeline.arn
+      input_artifacts = []
+      output_artifacts = ["source_output"]
+      configuration = {
+        RepositoryName = var.source_repository_name
+        BranchName     = var.source_branch_name!=""?var.source_branch_name:var.project_environment
+        OutputArtifactFormat: "CODE_ZIP"
+      }
+    }
+  }
+
+
+  stage {
+    name = "Terraform-Plan"
+    action {
+      category = "Build"
+      name     = "Terraform-Plan"
+      owner    = "AWS"
+      provider = "CodeBuild"
+      version  = "1"
+      input_artifacts = ["source_output"]
+      output_artifacts = ["tf_plan_output"]
+      configuration = {
+        ProjectName = aws_codebuild_project.plan-resource.name
+      }
+    }
+  }
+
+
+  stage {
+    name = "Manual-Approval"
+    action {
+      category = "Approval"
+      name     = "Approve-Terraform-Apply"
+      owner    = "AWS"
+      provider = "Manual"
+      version  = "1"
+      input_artifacts = []
+      output_artifacts = []
+      configuration = {
+        CustomData = "Review the Terraform plan before applying."
+      }
+    }
+  }
+
+
+  stage {
+    name = var.build_name
+    action {
+      category = var.build_category
+      name     = var.build_name
+      owner    = var.build_owner
+      provider = var.build_provider
+      version  = var.build_version
+      input_artifacts = ["tf_plan_output"]
+      output_artifacts = ["build_output"]
+      configuration = {
+       ProjectName=aws_codebuild_project.resource.name
+      }
+    }
+  }
+}
